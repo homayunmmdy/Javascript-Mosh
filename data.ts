c0f30e6a786ac3b38d4e893d9384940b19e710a4
Homayoun <homayoun763@gmail.com>
2024-02-09 02:59:29 +0330

    add first card

---
+++ data.ts
@@ -0,0 +1,12 @@
+import { CardsType } from "./types/collection";
+
+export const Cardsdata: CardsType[] = [
+  {
+    id: "1",
+    title: "multi-language website",
+    url: "https://github.com/homayunmmdy/Multi-language/assets/129702378/6e858b12-9e1a-4d1f-9e50-edd2a4371859",
+    tools: ["nextjs", "i18", "tailwindcss", "typescripts"],
+    live_demo: "https://multi-language-chi.vercel.app/pt",
+    source_code: "https://github.com/homayunmmdy/Multi-language",
+  },
+];
---
+++ types/collection.ts
@@ -0,0 +1,8 @@
+export interface CardsType {
+    id: string;
+    title: string;
+    url: string;
+    tools : string[] ;
+    live_demo : string ;
+    source_code: string
+  }
