501bc65859cf4fd58c28b0d889c1a26a7b4d9d5a
Homayoun <homayoun763@gmail.com>
2024-02-11 19:42:46 +0330

    Add 42 idea

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -14,21 +14,63 @@ const Projects = () => {
         <li>Text speech</li>
         <li>Device Battery Dectector</li>
         <li>Google auth</li>
+        <li>Spin wheel Game</li>
+        <li>Chart</li>
+        <li>BreakOut game</li>
+        <li>CryptoCurrecny Price App</li>
+        <li>Aspect Ratio Calculator</li>
+        <li>Typeing Test</li>
+        <li>Soduco</li>
+        <li>Github API</li>
+        <li>minesweeper</li>
+        <li>Recipe Guide App</li>
+        <li>Gradient Generator</li>
+        <li>Expenses Tracker</li>
+        <li>Chrome Translator Extension</li>
+        <li>Chrome Todo list Extension</li>
+        <li>Image Color Picker</li>
+        <li>2048 Game</li>
+        <li>Form Validation</li>
         <li>Newsletter</li>
+        <li>Calculator App</li>
+        <li>JSON Parcer</li>
+        <li>Alarm App</li>
+        <li>Chess</li>
+        <li>To do list</li>
+        <li>Flappy</li>
+        <li>Speech to text</li>
+        <li>Snake Game</li>
+        <li>Budgetting App</li>
+        <li>Life code editor</li>
+        <li>Word Guessing </li>
+        <li>Coin Flip Game</li>
         <li>QR Code generator</li>
         <li>Tik Tac Toe</li>
+        <li>Music player</li>
         <li>Map</li>
+        <li>Pong game</li>
+        <li>Authntication</li>
         <li>Restfull API</li>
+        <li>Movie Guide</li>
         <li>Password Generator</li>
         <li>Search ablitiy</li>
+        <li>Box shadow generator</li>
         <li>adding card</li>
+        <li>Currency Converter</li>
         <li>Drag and Drop Todo list</li>
+        <li>Event Calendar</li>
         <li>Transalte</li>
+        <li>Game release calender</li>
+        <li>Email sender app</li>
+        <li>Text Encryptor</li>
         <li>Multi step form</li>
         <li>Quizz app</li>
+        <li>Sudoku</li>
         <li>Video Player</li>
         <li>Image generator</li>
         <li>Chatbot</li>
+        <li>3D Model</li>
+        <li>Chat app</li>
         <li>Stack (DSA)</li>
       </ol>
     </div>
