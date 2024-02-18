8c3df3a0aac259d26802f1553b79e3bb67d06728
Homayoun <homayoun763@gmail.com>
2024-02-18 21:22:53 +0330

    Organize them

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Projects = () => {
   return (
-    <div >
+    <div>
       <div className="py-20 flex items-center justify-center">
         <div className="container">
           <div className="mx-auto max-w-4xl sm:text-center">
@@ -28,9 +28,30 @@ const Projects = () => {
               <div className="p-7 rounded-xl bg-amber-100 dark:bg-neutral-700/70">
                 <h3 className="text-xl font-semibold mb-7">Home page</h3>
 
-                <ol className="max-w-md space-y-1 text-gray-500 list-decimal list-inside dark:text-gray-400">
-                  <li className="font-medium leading-7 text-gray-500 mb-6 dark:text-gray-400">
-                    weather app
+                <ol className="max-w-md space-y-1 text-gray-500 list-decimal list-inside dark:text-gray-400 mb-4">
+                  <li className="font-medium text-gray-500 dark:text-gray-400">
+                    Text speech
+                  </li>
+                  <li className="font-medium text-gray-500 dark:text-gray-400">
+                    Google auth
+                  </li>
+                  <li className="font-medium text-gray-500 dark:text-gray-400">
+                    Chart
+                  </li>
+                  <li className="font-medium text-gray-500 dark:text-gray-400">
+                    CryptoCurrecny Price App
+                  </li>
+                  <li className="font-medium text-gray-500 dark:text-gray-400">
+                    Aspect Ratio Calculator
+                  </li>
+                  <li className="font-medium text-gray-500 dark:text-gray-400">
+                    Typeing Test
+                  </li>
+                  <li className="font-medium text-gray-500 dark:text-gray-400">
+                  Github API
+                  </li>
+                  <li className="font-medium text-gray-500 dark:text-gray-400">
+                  Recipe Guide App
                   </li>
                 </ol>
 
@@ -52,16 +73,50 @@ const Projects = () => {
                 </Link>
               </div>
             </div>
+            <div>
+              <div className="p-7 rounded-xl bg-yellow-100 dark:bg-neutral-700/70">
+                <h3 className="text-xl font-semibold mb-7">Extension</h3>
 
+                <ol className="max-w-md space-y-1 text-gray-500 list-decimal list-inside dark:text-gray-400 mb-4">
+                  <li className="font-medium text-gray-500 dark:text-gray-400">
+                  Chrome Todo list 
+                  </li>
+                  <li className="font-medium text-gray-500 dark:text-gray-400">
+                  Chrome Translator 
+                  </li>
+                  
+                </ol>
+
+                <Link
+                  href="/"
+                  className="py-3 flex items-center justify-center w-full font-semibold rounded-md bg-white hover:bg-purple-500 hover:text-white transition-all duration-500 dark:bg-neutral-900 dark:hover:bg-purple-500 dark:hover:text-white "
+                >
+                  Home Page
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 448 512"
+                    className=" h-5 w-5 ms-3"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"
+                    />
+                  </svg>
+                </Link>
+              </div>
+            </div>
             <div>
               <div className="p-7 rounded-xl bg-emerald-100 dark:bg-neutral-700/70">
-                <h3 className="text-xl font-semibold mb-7">
-                  DSA
-                </h3>
-                <ol className="max-w-md space-y-1 text-gray-500 list-decimal list-inside dark:text-gray-400">
-                  <li className="font-medium leading-7 text-gray-500 mb-6 dark:text-gray-400">
-                    weather app
-                  </li>
+                <h3 className="text-xl font-semibold mb-7">DSA</h3>
+                <ol className="max-w-md space-y-1 text-gray-500 list-decimal list-inside dark:text-gray-400 mb-4">
+                  <li className="font-medium text-gray-500 dark:text-gray-400">Graph</li>
+                  <li className="font-medium text-gray-500 dark:text-gray-400">Sorting</li>
+                  <li className="font-medium text-gray-500 dark:text-gray-400">Queues</li>
+                  <li className="font-medium text-gray-500 dark:text-gray-400">Trees</li>
+                  <li className="font-medium text-gray-500 dark:text-gray-400">Bubble sorting</li>
+                  <li className="font-medium text-gray-500 dark:text-gray-400">Searching</li>
+                  <li className="font-medium text-gray-500 dark:text-gray-400">Hash Table</li>
+                  <li className="font-medium text-gray-500 dark:text-gray-400">Linked List</li>
                 </ol>
                 <Link
                   href="/DSA"
@@ -84,9 +139,7 @@ const Projects = () => {
 
             <div>
               <div className="p-7 rounded-xl bg-red-100 dark:bg-neutral-700/70">
-                <h3 className="text-xl font-semibold mb-7">
-                  React
-                </h3>
+                <h3 className="text-xl font-semibold mb-7">React</h3>
                 <ol className="max-w-md space-y-1 text-gray-500 list-decimal list-inside dark:text-gray-400">
                   <li className="font-medium leading-7 text-gray-500 mb-6 dark:text-gray-400">
                     weather app
@@ -113,12 +166,10 @@ const Projects = () => {
 
             <div>
               <div className="p-7 rounded-xl bg-red-100 dark:bg-neutral-700/70">
-                <h3 className="text-xl font-semibold mb-7">
-                  Nodejs
-                </h3>
+                <h3 className="text-xl font-semibold mb-7">Nodejs</h3>
                 <ol className="max-w-md space-y-1 text-gray-500 list-decimal list-inside dark:text-gray-400">
                   <li className="font-medium leading-7 text-gray-500 mb-6 dark:text-gray-400">
-                    weather app
+                    Auth saved in db
                   </li>
                 </ol>
                 <Link
@@ -142,12 +193,24 @@ const Projects = () => {
 
             <div>
               <div className="p-7 rounded-xl bg-amber-100 dark:bg-neutral-700/70">
-                <h3 className="text-xl font-semibold mb-7">
-                  Games
-                </h3>
-                <p className="font-medium leading-7 text-gray-500 mb-6 dark:text-gray-400">
-                  Comming Soon
-                </p>
+                <h3 className="text-xl font-semibold mb-7">Games</h3>
+                <ol className="max-w-md space-y-1 text-gray-500 list-decimal list-inside dark:text-gray-400 mb-4">
+                  <li className="font-medium text-gray-500 dark:text-gray-400">
+                    Spin wheel Game
+                  </li>
+                  <li className="font-medium text-gray-500 dark:text-gray-400">
+                    BreakOut game
+                  </li>
+                  <li className="font-medium text-gray-500 dark:text-gray-400">
+                  Soduco
+                  </li>
+                  <li className="font-medium text-gray-500 dark:text-gray-400">
+                  minesweeper
+                  </li>
+                  <li className="font-medium text-gray-500 dark:text-gray-400">
+                  2048 Game
+                  </li>
+                </ol>
                 <Link
                   href="#"
                   className="py-3 flex items-center justify-center w-full font-semibold rounded-md bg-white hover:bg-purple-500 hover:text-white transition-all duration-500 dark:bg-neutral-900 dark:hover:bg-purple-500 dark:hover:text-white "
@@ -169,12 +232,11 @@ const Projects = () => {
 
             <div>
               <div className="p-7 rounded-xl bg-emerald-100 dark:bg-neutral-700/70">
-                <h3 className="text-xl font-semibold mb-7">
-                  Full UI
-                </h3>
-                <p className="font-medium leading-7 text-gray-500 mb-6 dark:text-gray-400">
-                  Share your project idea or features with us.
-                </p>
+                <h3 className="text-xl font-semibold mb-7">Full UI</h3>
+                <ol className="max-w-md space-y-1 text-gray-500 list-decimal list-inside dark:text-gray-400 mb-4">
+                  <li className="font-medium text-gray-500 dark:text-gray-400">Blog Home page</li>
+                  <li className="font-medium text-gray-500 dark:text-gray-400">E-commerce Home page</li>
+                </ol>
                 <Link
                   href="/"
                   className="py-3 flex items-center justify-center w-full font-semibold rounded-md bg-white hover:bg-purple-500 hover:text-white transition-all duration-500 dark:bg-neutral-900 dark:hover:bg-purple-500 dark:hover:text-white "
@@ -196,30 +258,7 @@ const Projects = () => {
           </div>
         </div>
       </div>
-      <h2 className="mb-2 text-lg font-semibold text-gray-900 dark:text-white">
-        List of the project will add
-      </h2>
-      <ol className="max-w-md space-y-1 text-gray-500 list-decimal list-inside dark:text-gray-400">
-        <li>weather app</li>
-        <li>Text speech</li>
-        <li>Google auth</li>
-        <li>Spin wheel Game</li>
-        <li>Chart</li>
-        <li>react typescript tailwind starter</li>
-        <li>BreakOut game</li>
-        <li>CryptoCurrecny Price App</li>
-        <li>Aspect Ratio Calculator</li>
-        <li>Typeing Test</li>
-        <li>Soduco</li>
-        <li>Github API</li>
-        <li>minesweeper</li>
-        <li>Recipe Guide App</li>
-        <li>Expenses Tracker</li>
-        <li>Chrome Translator Extension</li>
-        <li>Chrome Todo list Extension</li>
-        <li>Image Color Picker</li>
-        <li>2048 Game</li>
-        <li>Form Validation</li>
+      {/* <ol className="max-w-md space-y-1 text-gray-500 list-decimal list-inside dark:text-gray-400">
         <li>Newsletter</li>
         <li>Calculator App</li>
         <li>JSON Parcer</li>
@@ -247,26 +286,17 @@ const Projects = () => {
         <li>Drag and Drop Todo list</li>
         <li>Event Calendar</li>
         <li>Transalte</li>
-        <li>Graph (DSA)</li>
-        <li>Sorting (DSA)</li>
-        <li>Searching (DSA)</li>
-        <li>Hash Table (DSA)</li>
-        <li>Linked List (DSA)</li>
-        <li>Queues (DSA)</li>
-        <li>Trees (DSA)</li>
-        <li>Bubble sorting (DSA)</li>
         <li>Game release calender</li>
         <li>Email sender app</li>
         <li>Text Encryptor</li>
         <li>Multi step form</li>
-        <li>Quizz app</li>
         <li>Sudoku</li>
         <li>Video Player</li>
         <li>Image generator</li>
         <li>Chatbot</li>
         <li>3D Model</li>
         <li>Chat app</li>
-      </ol>
+      </ol> */}
     </div>
   );
 };
