1a16c58bdc1b081ee08a6c9a2110b2d5e817f1b4
Homayoun <homayoun763@gmail.com>
2024-04-11 20:16:30 +0330

    Hero sec

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Hero from './components/Hero'
 
 const Home = () => {
   return (
     <div style={{background: "linear-gradient(20deg, hsl(130deg, 8%, 14%) 40%, hsl(0deg, 0%, 13%) 100%)"}}>
-      <div className=' w-[92%] mx-auto' >Home</div>
+      <div className=' w-[92%] mx-auto' >
+        <Hero />
+      </div>
     </div>
   )
 }
