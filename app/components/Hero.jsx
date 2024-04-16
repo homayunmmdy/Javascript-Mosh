6350e1680d1c0e131e3600ec86ebca770e39996a
Homayoun <homayoun763@gmail.com>
2024-04-16 20:18:34 +0330

    fix bug

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <div>
       <div className="h-full w-full">
-        <div className="mx-auto max-w-7xl py-10">
+        <div className="mx-auto py-10">
           <div className="grid grid-cols-12 gap-4">
             <div className="col-span-12 md:col-span-7 xl:col-span-8">
               <div>
