b94fd44c86a8cdba972aa0323064e4b13ac3c022
Homayoun <homayoun763@gmail.com>
2024-04-22 20:09:32 +0330

    add client zero

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,30 +1,18 @@
 import { Inter } from "next/font/google";
 import "./Font.css";
 import "./globals.css";
-import SiteConfig from "@/app/config/site"
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import GetStarted from "./components/GetStarted";
-
+import  SiteConfig from "@/app/config/site"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: `${SiteConfig.name}`,
+  title: `${SiteConfig.name} | ${SiteConfig.name_en}`,
   description: SiteConfig.description,
 };
+
 export default function RootLayout({ children }) {
   return (
     <html lang={SiteConfig.lang} dir={SiteConfig.dir}>
-      <body className={inter.className}>
-        <Navbar />
-        {children}
-        <div style={{ background: "hsla(127, 8%, 14%, 1)" }}>
-        <div className='px-5 w-[98%] md:w-[92%] mx-auto'>
-          <GetStarted />
-        </div>
-      </div>
-        <Footer />
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
