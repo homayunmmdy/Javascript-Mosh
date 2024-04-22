0defbff490dc2cebdff77f7cbad0b6a3d86093bb
Homayoun <homayoun763@gmail.com>
2024-04-22 23:57:51 +0330

    fix the link of the gallery

--- app/components/Gallery.jsx
+++ app/components/Gallery.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from "next/link";
 
 const Gallery = () => {
   return (
@@ -7,37 +8,37 @@ const Gallery = () => {
 	<div className="py-4 px-2 mx-auto max-w-screen-xl sm:py-4 lg:px-6">
 		<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4 h-full">
 			<div className="col-span-2 sm:col-span-1 md:col-span-2 bg-gray-50 h-auto md:h-full flex flex-col">
-				<a href="" className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 flex-grow">
+				<Link href="/ai" className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 flex-grow">
 					<img src="/img4.jpg" alt="" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"/>
 					<div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
 					<h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl">AI</h3>
-				</a>
+				</Link>
 			</div>
 			<div className="col-span-2 sm:col-span-1 md:col-span-2 bg-stone-50">
-				<a href="" className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 mb-4">
+				<Link href="/content_creation" className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 mb-4">
 					<img src="/img1.jpg" alt="" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"/>
 					<div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
 					<h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl">Content Creation</h3>
-				</a>
+				</Link>
 				<div className="grid gap-4 grid-cols-2 sm:grid-cols-2 lg:grid-cols-2">
-					<a href="" className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40">
+					<Link href="coding" className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40">
 						<img src="/img2.jpg" alt="" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"/>
 						<div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
 						<h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl">Coding</h3>
-					</a>
-					<a href="" className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40">
+					</Link>
+					<Link href="life" className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40">
 						<img src="/img5.jpg" alt="" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"/>
 						<div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
 						<h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl">Life</h3>
-					</a>
+					</Link>
 				</div>
 			</div>
 			<div className="col-span-2 sm:col-span-1 md:col-span-1 bg-sky-50 h-auto md:h-full flex flex-col">
-				<a href="" className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 flex-grow">
+				<Link href="business" className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 flex-grow">
 					<img src="/img3.jpg" alt="" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"/>
 					<div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
 					<h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl">Business</h3>
-				</a>
+				</Link>
 			</div>
 		</div>
 	</div>
