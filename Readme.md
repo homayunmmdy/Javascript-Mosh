654813473305fda758af10206ba12bcf7603cab2
Homayoun <homayoun763@gmail.com>
2024-09-12 18:30:47 +0330

    Logicla operators

--- Readme.md
+++ Readme.md
@@ -171,3 +171,25 @@ let points = 100;
 let type = points > 100 ? "gold" : "silver";
 console.log(type)
 ```
+
+## logicla operators
+
+```javascript 
+// Logical AND (&&)
+// Return true if both operands are true
+let hightIncome = true;
+let goodCreditScore = false;
+let credit = hightIncome && goodCreditScore;
+console.log(credit)
+
+// Logincal OR (||)
+// Return true if one of the operands is true
+let eligibleForLoan = hightIncome || credit;
+console.log(false || true)
+
+//NOT (!)
+// turn true to false and false to true
+let applicationRefused =  !eligibleForLoan
+
+console.log(applicationRefused)
+```
--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-// if a customer has more thna 100 points,
-// they are a 'gold' customer , otherwise,
-// they are a 'silver' customer
+// Logical AND (&&)
+// Return true if both operands are true
+let hightIncome = true;
+let goodCreditScore = false;
+let credit = hightIncome && goodCreditScore;
+console.log(credit)
 
-let points = 100;
-let type = points > 100 ? "gold" : "silver";
-console.log(type)
+// Logincal OR (||)
+// Return true if one of the operands is true
+let eligibleForLoan = hightIncome || credit;
+console.log(false || true)
+
+//NOT (!)
+// turn true to false and false to true
+let applicationRefused =  !eligibleForLoan
+
+console.log(applicationRefused)
