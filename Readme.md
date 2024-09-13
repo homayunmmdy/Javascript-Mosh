2f466b6459349f72a0d7797b14a44ac8911b1d01
Homayoun <homayoun763@gmail.com>
2024-09-13 23:03:32 +0330

    Exercise - Grade

--- Readme.md
+++ Readme.md
@@ -462,3 +462,32 @@ function sum(limit) {
   return sum;
 }
 ```
+
+### Exercise - Grade
+
+```javascript
+const marks = [90, 70, 80];
+
+console.log(claculateGrade(marks));
+// 0-59: F
+// 60-69: D
+// 70-79: C
+// 80-89: B
+// 90-100: A
+
+function claculateGrade(mark) {
+  const average = calculateAverage(marks);
+
+  if (average < 60) return "F";
+  if (average < 70) return "D";
+  if (average < 80) return "C";
+  if (average < 90) return "B";
+  return "A";
+}
+
+function calculateAverage(array) {
+  let sum = 0;
+  for (let mark of array) sum += mark;
+  return sum / array.length;
+}
+```
--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-console.log(sum(10));
+const marks = [90, 70, 80];
 
-function sum(limit) {
+console.log(claculateGrade(marks));
+// 0-59: F
+// 60-69: D
+// 70-79: C
+// 80-89: B
+// 90-100: A
+
+function claculateGrade(mark) {
+  const average = calculateAverage(marks);
+
+  if (average < 60) return "F";
+  if (average < 70) return "D";
+  if (average < 80) return "C";
+  if (average < 90) return "B";
+  return "A";
+}
+
+function calculateAverage(array) {
   let sum = 0;
-
-  for (let i = 0; i <= limit; i++)
-    if (i % 3 === 0 || i % 5 === 0) {
-      sum += i;
-    }
-  return sum;
+  for (let mark of array) sum += mark;
+  return sum / array.length;
 }
