9b3a3b436045618d29eebe4b23534aef6a5c12e1
Homayoun <homayoun763@gmail.com>
2024-09-13 19:03:46 +0330

    check speed example

--- Readme.md
+++ Readme.md
@@ -354,3 +354,27 @@ function fizzBuzz(input) {
   return input;
 }
 ```
+
+### check speed example
+```javascript
+// Speed Limit = 70
+// 5 -> 1 point
+// Math.floor(1.3)
+// 12 points -< suspended
+checkSpeed(130);
+function checkSpeed(speed) {
+  const speedLimit = 70;
+  const kmPerPoint = 5;
+
+  if (speed < speedLimit + kmPerPoint) {
+    console.log("OK");
+    return;
+  }
+  const points = Math.floor((speed - speedLimit) / kmPerPoint);
+  if (points >= 12) {
+    console.log("license suspended");
+  } else {
+    console.log("Points", points);
+  }
+}
+```
--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-// Divisible by 3 => Fizz
-// Divisible by 5 => Buzz
-// Divisible by 3 and 5 => input
-// Not  a number => "Not a number"
-const output = fizzBuzz(15);
-console.log(output);
-function fizzBuzz(input) {
-  if (typeof input !== "number") return NaN;
+// Speed Limit = 70
+// 5 -> 1 point
+// Math.floor(1.3)
+// 12 points -< suspended
+checkSpeed(130);
+function checkSpeed(speed) {
+  const speedLimit = 70;
+  const kmPerPoint = 5;
 
-  if (input % 3 === 0 && input % 5 === 0) return "FizzBuzz";
-  if (input % 3 === 0) return "Fizz";
-  if (input % 5 === 0) return "Buzz";
-
-  return input;
+  if (speed < speedLimit + kmPerPoint) {
+    console.log("OK");
+    return;
+  }
+  const points = Math.floor((speed - speedLimit) / kmPerPoint);
+  if (points >= 12) {
+    console.log("license suspended");
+  } else {
+    console.log("Points", points);
+  }
 }
