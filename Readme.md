8170745338fee86763656014b1eece9c1789c0c9
Homayoun <homayoun763@gmail.com>
2024-09-13 23:27:25 +0330

    isPrime example

--- Readme.md
+++ Readme.md
@@ -508,3 +508,20 @@ function showStart(rows) {
   }
 }
 ```
+
+### isPrime example
+
+```javascript
+showPrimes(20);
+function showPrimes(limit) {
+  for (let number = 2; number <= limit; number++)
+    if (isPrime(number)) console.log(number);
+}
+
+function isPrime(number) {
+  for (let factor = 2; factor < number; factor++)
+    if (number % factor === 0) return false;
+
+  return true;
+}
+```
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-showStart(12);
+showPrimes(20);
+function showPrimes(limit) {
+  for (let number = 2; number <= limit; number++)
+    if (isPrime(number)) console.log(number);
+}
 
-function showStart(rows) {
-  for (let row = 0; row <= rows; row++) {
-    let pattern = "";
-    for (let i = 0; i < row; i++) {
-      pattern += "*";
-    }
-    console.log(pattern);
-  }
+function isPrime(number) {
+  for (let factor = 2; factor < number; factor++)
+    if (number % factor === 0) return false;
+
+  return true;
 }
