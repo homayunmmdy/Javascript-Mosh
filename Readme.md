955a3d117f77e231d11161a5e89e6072b22e4082
Homayoun <homayoun763@gmail.com>
2024-09-16 21:02:11 +0330

    Exercise 4- Blog Post Object

--- Readme.md
+++ Readme.md
@@ -821,3 +821,21 @@ function areSame(address1, address2) {
   return address1 === address2;
 }
 ```
+
+### Exercise 4- Blog Post Object
+
+```javascript
+const Post = {
+  title: "Biggest Mistake in Tech Industry",
+  body: "This is the sample text for the body of this great blog",
+  auth: "Homayoun",
+  views: 2000,
+  comments: [
+    { name: "John", body: "Great Article" },
+    { name: "Alex", body: "Perfect Article" },
+  ],
+  isLive: true
+};
+
+console.log(Post)
+```
--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-const address1 = new Address("Wall Street", "NYC", 1001);
-const address2 = new Address("Wall Street", "NYC", 1001);
-const address3 = address1;
+const Post = {
+  title: "Biggest Mistake in Tech Industry",
+  body: "This is the sample text for the body of this great blog",
+  auth: "Homayoun",
+  views: 2000,
+  comments: [
+    { name: "John", body: "Great Article" },
+    { name: "Alex", body: "Perfect Article" },
+  ],
+  isLive: true
+};
 
-console.log("are Equal", areEqual(address1, address2));
-console.log("are Same", areSame(address1, address2));
-console.log("are Same2", areSame(address1, address3));
-
-function Address(street, city, zipCode) {
-  (this.street = street), (this.city = city), (this.zipCode = zipCode);
-}
-
-function areEqual(address1, address2) {
-  return (
-    address1.street === address2.street &&
-    address1.city === address2.city &&
-    address1.zipCode === address2.zipCode
-  );
-}
-
-function areSame(address1, address2) {
-  return address1 === address2;
-}
+console.log(Post)
