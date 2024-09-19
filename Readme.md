1a37ab422b4dc27c7340f833f3269a49b73750e0
Homayoun <homayoun763@gmail.com>
2024-09-19 17:46:44 +0330

    Reducing an Array

--- Readme.md
+++ Readme.md
@@ -1100,3 +1100,23 @@ const items = numbers
 
 console.log(items);
 ```
+
+## Reducing an Array
+if you don't provide the second element it will pick the first to starter
+```javascript
+const numbers = [10, -1, 2, 3];
+
+let sum = 0;
+for (let n of numbers) sum += n;
+
+console.log(sum);
+
+// a = 0 , c = 1 => a = 1
+// a = 1 , c = -1 => a = 0
+// a = 0 , c = 2 => a = 2
+// a = 2 , c = 3 => a = 5
+const result = numbers.reduce((a, c) => a + c);
+
+console.log(result);
+
+```
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const numbers = [1, -1, 2, 3];
+const numbers = [10, -1, 2, 3];
 
-const items = numbers
-  .filter((value) => value >= 0)
-  .map((n) => ({ value: n }))
-  .filter((obj) => obj.value > 1)
-  .map((obj) => obj.value);
+let sum = 0;
+for (let n of numbers) sum += n;
 
-console.log(items);
+console.log(sum);
+
+// a = 0 , c = 1 => a = 1
+// a = 1 , c = -1 => a = 0
+// a = 0 , c = 2 => a = 2
+// a = 2 , c = 3 => a = 5
+const result = numbers.reduce((a, c) => a + c);
+
+console.log(result);
