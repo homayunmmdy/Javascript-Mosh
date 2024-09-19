bb54d0d0d73a4d5074832f90df725b47cb661ec0
Homayoun <homayoun763@gmail.com>
2024-09-19 17:53:02 +0330

    Exercise 1- Array from Range

--- Readme.md
+++ Readme.md
@@ -1120,3 +1120,19 @@ const result = numbers.reduce((a, c) => a + c);
 console.log(result);
 
 ```
+
+## Exercise 1- Array from Range
+```javascript
+const numbers = arrayFormRage(1, 20);
+
+console.log(numbers);
+
+function arrayFormRage(min, max) {
+  const output = [];
+  for (let i = min; i <= max; i++) {
+    output.push(i);
+  }
+  return output;
+}
+
+```
--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const numbers = [10, -1, 2, 3];
+const numbers = arrayFormRage(1, 20);
 
-let sum = 0;
-for (let n of numbers) sum += n;
+console.log(numbers);
 
-console.log(sum);
-
-// a = 0 , c = 1 => a = 1
-// a = 1 , c = -1 => a = 0
-// a = 0 , c = 2 => a = 2
-// a = 2 , c = 3 => a = 5
-const result = numbers.reduce((a, c) => a + c);
-
-console.log(result);
+function arrayFormRage(min, max) {
+  const output = [];
+  for (let i = min; i <= max; i++) {
+    output.push(i);
+  }
+  return output;
+}
