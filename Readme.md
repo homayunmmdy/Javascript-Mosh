e1eacabf11a5e7be63c503f8b451c21a52a1fcf0
Homayoun <homayoun763@gmail.com>
2024-09-23 00:13:51 +0330

    Exercise 3- Error Handling

--- Readme.md
+++ Readme.md
@@ -1586,3 +1586,25 @@ const circle = {
 
 console.log(circle.area)
 ```
+
+### Exercise 3- Error Handling
+
+```javascript
+try {
+  const numbers = [1, 2, 3, 4];
+  const count = countOccurrences(null, 1);
+  console.log(count);
+} catch (e) {
+  console.log(e.message);
+}
+
+function countOccurrences(array, searchElement) {
+  if (!Array.isArray(array)) throw new Error("Inalid array.");
+
+  return array.reduce((accumulator, current) => {
+    const occurrence = current === searchElement ? 1 : 0;
+    return accumulator + occurrence;
+  }, 0);
+}
+
+```
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const circle = {
-  radius: 1,
-  get area() {
-    return Math.PI * this.radius * this.radius;
-  },
-};
+try {
+  const numbers = [1, 2, 3, 4];
+  const count = countOccurrences(null, 1);
+  console.log(count);
+} catch (e) {
+  console.log(e.message);
+}
 
-console.log(circle.area)
+function countOccurrences(array, searchElement) {
+  if (!Array.isArray(array)) throw new Error("Inalid array.");
+
+  return array.reduce((accumulator, current) => {
+    const occurrence = current === searchElement ? 1 : 0;
+    return accumulator + occurrence;
+  }, 0);
+}
